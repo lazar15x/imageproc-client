9c6ff7452c819479e4fdb1fa7eb85f6a08b3ad4b
lazar15x <166030992+lazar15x@users.noreply.github.com>
2025-04-25 17:46:12 +0300

    chore: add aliases, prettier + cfg

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+const fullPath = (str: string) => path.resolve(__dirname, str);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': fullPath('./src'),
+    },
+  },
+});
